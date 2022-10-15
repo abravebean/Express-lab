c2637e2a588a4f5951128cff8292d1e4325b4a5a
unknown <kodytran854@gmail.com>
2022-10-15 19:01:57 -0400

    commit

--- server.js
+++ server.js
@@ -1,51 +1,27 @@
-const express = require('express')
-const app = express()
-
-app.get("/greeting/", (req, res) => {
-    res.send(`Hello Stranger`)
-})
-
-// Greetings
-app.get("/greeting/:name", (req, res) => {
-    res.send(`"Whats up, ${(req.params.name)}! It's so great to see you!"`)
-})
-
-
-// Tip Calculator
-app.get("/tip/:total/:tipPercentage/", (req, res) => {
-    res.send(`${ parseInt(req.params.tipPercentage)  * parseInt(req.params.total) / 100} `)
-})
-
-
-
-// Magic 8 Ball
-app.get("/magic/:question/", (req, res) => {
-    const responses = [
-        "It is certain", 
-        "It is decidedly so", 
-        "Without a doubt", 
-        "Yes definitely",
-        "You may rely on it", 
-        "As I see it yes", 
-        "Most likely", 
-        "Outlook good",
-        "Yes", 
-        "Signs point to yes", 
-        "Reply hazy try again", 
-        "Ask again later",
-        "Better not tell you now", 
-        "Cannot predict now", 
-        "Concentrate and ask again",
-        "Don't count on it", 
-        "My reply is no", 
-        "My sources say no",
-        "Outlook not so good",
-        "Very doubtful"
-    ]
-    res.send(`<h1>${req.params.question} = ${responses[Math.floor(Math.random()*responses.length)]}</h1>`)
-})
-
-
-app.listen(3000, () => {
-    console.log(`Hello! I'm listening on port 3000...`)
+const express = require("express");
+const app = express();
+ const eightBall =
+        ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
+
+app.get("/greeting", (req, res) => {
+    res.send("Hello Stranger!");
+});
+app.get("/:name?", (req, res) => {
+    res.send("Hello " + req.params.name + " it's great to see you");
+});
+
+app.get("/tip/:total/:tipPercentage", function (req, res) {
+    res.send("your tip is: $" + req.params.total * (req.params.tipPercentage / 100))
+    // res.send((req.params.total + req.params.tipPercentage))
+});
+const randomResponse = eightBall[Math.floor(Math.random() * eightBall.length)];
+
+
+app.get("/magic/:question", function (req, res) {
+    res.send(`${req.params.question} <h1>${randomResponse}</h1>`)
+});
+
+app.listen(3000)
